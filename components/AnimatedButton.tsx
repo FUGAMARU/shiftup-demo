be0cf8be13076d938ed0dbdd6d0502ce44dddfd9
FUGAMARU <gofugashi@gmail.com>
2022-07-23 17:27:09 +0900

    [Add] 送信ボタン押下時に文字がスピナーに切り替わるように

--- components/AnimatedButton.tsx
+++ components/AnimatedButton.tsx
@@ -1,8 +1,28 @@
+// React Hooks
+import { useState } from "react"
+
+// Chakra UI Components
+import { Flex, Text, Spinner } from "@chakra-ui/react"
+
 // CSS Modules
 import styles from "../styles/AnimatedButton.module.css"
 
 const AnimatedButton = (props: { text: string }) => {
-  return <button className={styles.animatedButton}>{props.text}</button>
+  const [animationTrigger, setAnimationTrigger] = useState(false)
+  const [textClassSwitcher, setTextClassSwitcher] = useState(false)
+
+  const handleButtonClick = () => {
+    setTextClassSwitcher(true)
+    setTimeout(() => {
+      setAnimationTrigger(true)
+    }, 700)
+  }
+
+  return (
+    <Flex className={styles.animatedButton} w="13rem" h="3rem" textAlign="center" justifyContent="center" alignItems="center" onClick={handleButtonClick}>
+      {!!!animationTrigger ? <Text className={textClassSwitcher ? "animate__animated animate__fadeOutUp animate__fast" : ""}>{props.text}</Text> : <Spinner />}
+    </Flex>
+  )
 }
 
 export default AnimatedButton
