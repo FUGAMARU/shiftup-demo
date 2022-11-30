0d76eb5beb3e707eea47b875f96fbd09e8c8fc37
FUGAMARU <gofugashi@gmail.com>
2022-11-30 11:07:26 +0900

    [Update] 認可ユーザー追加画面のバリデーションチェック方法を変更

--- pages/management/add-approved-user.tsx
+++ pages/management/add-approved-user.tsx
@@ -3,7 +3,7 @@ import { NextPage, InferGetStaticPropsType } from "next"
 import Head from "next/head"
 
 // React Hooks
-import { useState, useRef } from "react"
+import { useState, useRef, useEffect } from "react"
 
 // Chakra UI Components
 import { Flex, Grid, Text, Box, Input, Select, Radio, RadioGroup, Stack, Popover, PopoverTrigger, PopoverContent, PopoverArrow, PopoverBody } from "@chakra-ui/react"
@@ -29,7 +29,8 @@ import { SendButtonState } from "../../types/SendButtonState"
 // Importing Symbols
 import * as fs from "fs"
 import * as path from "path"
-type Symbols = { [key: string]: string }
+type Departments = { [department: string]: string }
+type Symbols = { [college: string]: Departments }
 type Props = InferGetStaticPropsType<typeof getStaticProps>
 
 export const getStaticProps = async () => {
@@ -43,10 +44,13 @@ export const getStaticProps = async () => {
 }
 
 const AddApprovedUser: NextPage<Props> = ({ symbols }) => {
+  const patternNEEC = /G\d{3}[A-Z]\d{4}/
+  const patternTUT = /[\dA-Z]{3}\d{5}/
   const [sendButtonState, setSendButtonState] = useState<SendButtonState>("text")
 
   // 学籍番号入力欄
-  const studentIdNumberRef = useRef<HTMLInputElement>(null)
+  const [studentIdNumberInput, setStudentIdNumberInput] = useState("")
+  const [isInputTUT, setInputTUT] = useState(false)
   const [studentIdNumberPopover, setStudentIdNumberPopover] = useState({
     isOpen: false,
     message: ""
@@ -61,6 +65,8 @@ const AddApprovedUser: NextPage<Props> = ({ symbols }) => {
 
   // 学部・学科プルダウンメニュー
   const departmentMenuRef = useRef<HTMLSelectElement>(null)
+  const [croppedSymbols, setCroppedSymbols] = useState<Symbols | Departments>(symbols)
+  const [isDisabledDepartmentMenu, setDisabledDepartmentMenu] = useState(true)
   const [departmentMenuPopover, setDepartmentMenuPopover] = useState({
     isOpen: false,
     message: ""
@@ -74,18 +80,44 @@ const AddApprovedUser: NextPage<Props> = ({ symbols }) => {
   }
 
   // 役職選択ラジオボタン
-  //const positionSelectorRef = useRef(null)
   const [userAttribute, setUserAttribute] = useState("Cast")
 
+  // 入力されている学籍番号によって選択できる学部・学科を切り替える
+  useEffect(() => {
+    setStudentIdNumberInput((current) => toHankaku(current).toUpperCase())
+    if (patternNEEC.test(studentIdNumberInput)) {
+      setInputTUT(false)
+      setDisabledDepartmentMenu(false)
+
+      const copiedSymbols = Object.assign({}, symbols)
+      delete copiedSymbols["東京工科大学"]
+      setCroppedSymbols(copiedSymbols)
+
+      return
+    }
+
+    if (patternTUT.test(studentIdNumberInput)) {
+      setInputTUT(true)
+      setDisabledDepartmentMenu(false)
+
+      const copiedSymbols = Object.assign({}, symbols)
+      setCroppedSymbols(copiedSymbols["東京工科大学"])
+
+      return
+    }
+
+    setInputTUT(false)
+    setDisabledDepartmentMenu(true)
+  }, [studentIdNumberInput])
+
   const checkValidation = (): boolean => {
-    if (!!!studentIdNumberRef.current || !!!departmentMenuRef.current) return false
+    if (!!!departmentMenuRef.current) return false
 
     let valid = true
 
-    studentIdNumberRef.current.value = toHankaku(studentIdNumberRef.current.value).toUpperCase()
-    const pattern = /^([a-zA-Z0-9]{1,})$/
+    setStudentIdNumberInput((current) => toHankaku(current).toUpperCase())
 
-    if (!!!pattern.test(studentIdNumberRef.current.value)) {
+    if (!!!patternNEEC.test(studentIdNumberInput) || !!!patternTUT.test(studentIdNumberInput)) {
       setStudentIdNumberPopover({
         isOpen: true,
         message: "学籍番号が正しく入力されていません"
@@ -105,14 +137,14 @@ const AddApprovedUser: NextPage<Props> = ({ symbols }) => {
   }
 
   const handleSendButtonClick = async () => {
-    if (!!!checkValidation() || !!!studentIdNumberRef.current || !!!departmentMenuRef.current) return
+    if (!!!checkValidation() || !!!departmentMenuRef.current) return
 
     setSendButtonState("spinner")
 
     await standBy(1000)
 
     const requestBody = {
-      studentNumber: studentIdNumberRef.current.value,
+      studentNumber: studentIdNumberInput,
       department: departmentMenuRef.current.value,
       position: userAttribute
     }
@@ -123,8 +155,8 @@ const AddApprovedUser: NextPage<Props> = ({ symbols }) => {
       if (res.status === 201) {
         setSendButtonState("checkmark")
         setTimeout(() => {
-          if (!!!studentIdNumberRef.current || !!!departmentMenuRef.current) return
-          studentIdNumberRef.current.value = ""
+          if (!!!departmentMenuRef.current) return
+          setStudentIdNumberInput("")
           departmentMenuRef.current.value = ""
           setUserAttribute("Cast")
         }, 1000)
@@ -162,7 +194,7 @@ const AddApprovedUser: NextPage<Props> = ({ symbols }) => {
             <Flex pl={resp(8, 16, 16)} py={5} alignItems="center">
               <Popover isOpen={studentIdNumberPopover.isOpen}>
                 <PopoverTrigger>
-                  <Input className="ksb" w={resp(250, 350, 350)} placeholder="(入力例) G021C1234" ref={studentIdNumberRef} bg="white" focusBorderColor="#48c3eb" isInvalid={studentIdNumberPopover.isOpen} errorBorderColor="red.200" onClick={closeStudentIdNumberPopover}></Input>
+                  <Input className="ksb" w={resp(250, 350, 350)} placeholder="(入力例) G021C1234" bg="white" focusBorderColor="#48c3eb" isInvalid={studentIdNumberPopover.isOpen} errorBorderColor="red.200" value={studentIdNumberInput} onChange={(e) => setStudentIdNumberInput(e.target.value)} onClick={closeStudentIdNumberPopover}></Input>
                 </PopoverTrigger>
                 <PopoverContent>
                   <PopoverArrow bg="red.100" />
@@ -182,13 +214,17 @@ const AddApprovedUser: NextPage<Props> = ({ symbols }) => {
             <Flex pl={resp(9, 16, 16)} py={5} alignItems="center">
               <Popover isOpen={departmentMenuPopover.isOpen}>
                 <PopoverTrigger>
-                  <Select w={resp(245, 350, 350)} placeholder="学科を選択" ref={departmentMenuRef} focusBorderColor="#48c3eb" isInvalid={departmentMenuPopover.isOpen} errorBorderColor="red.200" onClick={closeDepartmentMenuPopover}>
-                    {Object.keys(symbols).map((label, idx) => {
+                  <Select w={resp(245, 350, 350)} placeholder={`${isInputTUT ? "学部" : "学科"}を選択`} ref={departmentMenuRef} focusBorderColor="#48c3eb" isInvalid={departmentMenuPopover.isOpen} isDisabled={isDisabledDepartmentMenu} errorBorderColor="red.200" onClick={closeDepartmentMenuPopover}>
+                    {isInputTUT ? Object.keys(croppedSymbols).map((key, idx) => {
+                      return (
+                        <option value={key} key={idx}>{Object.values(croppedSymbols[key])}</option>
+                      )
+                    }) : Object.keys(croppedSymbols).map((label, idx) => {
                       return (
                         <optgroup label={label} key={idx}>
-                          {Object.keys(symbols[label]).map((subKey, subIdx) => {
+                          {Object.keys(croppedSymbols[label]).map((subKey, subIdx) => {
                             return (
-                              <option value={subKey} key={idx + subIdx}>{Object.values(symbols[label])[subIdx]}</option>
+                              <option value={subKey} key={idx + subIdx}>{Object.values(croppedSymbols[label])[subIdx]}</option>
                             )
                           })}
                         </optgroup>
