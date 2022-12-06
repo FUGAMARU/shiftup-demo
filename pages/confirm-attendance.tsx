b3c80424c74fb0331238bc78cf16164d7c849bc4
FUGAMARU <gofugashi@gmail.com>
2022-12-06 12:03:22 +0900

    [Update] 出勤確定処理画面のUIを他ページの管理系UIに統一

--- pages/confirm-attendance.tsx
+++ pages/confirm-attendance.tsx
@@ -6,12 +6,10 @@ import Head from "next/head"
 import useResponsive from "../hooks/useResponsive"
 
 // Chakra UI Components
-import { Box, Flex, Text, SimpleGrid } from "@chakra-ui/react"
+import { Box, Flex, Text, SimpleGrid, StackDivider, VStack, Button } from "@chakra-ui/react"
 
 // Custom Components
 import Body from "../components/Body"
-import ConfirmButton from "../components/button/ConfirmButton"
-import CancelButton from "../components/button/CancelButton"
 
 // Functions
 import { resp } from "../functions"
@@ -28,30 +26,41 @@ const ConfirmAttendance: NextPage = () => {
         <title>出勤確定処理 | ShiftUP!</title>
       </Head>
 
-      <Body title="出勤確定処理" statusMessage="出勤確定待ちの日程が2件あります">
-        <>
-          <SimpleGrid w={resp("100%", "80%", "80%")} mx="auto" my={5} py={3} columns={{ sm: 1, md: 2, lg: 2 }} spacingX={5} justifyItems="center" alignItems="center" boxShadow="xl" borderRadius={15} borderTop="solid 3px #718eed">
-            <Box textAlign={responsiveType === "SmartPhone" ? "center" : "right"}>
-              <Text className="kr" maxW={resp("17rem", "13.5rem", "18rem")} fontSize={resp("0.75rem", "0.80rem", "0.85rem")} color="#5f5f5f">12月シフト募集</Text>
-              <Text className="kb" fontSize="1.2rem">2022/12/11 (日)</Text>
-            </Box>
-            <Flex mr={2} mt={resp(2, 0, 0)}>
-              <Box mr={3}><ConfirmButton text="出勤確定" /></Box>
-              <Box ml={3}> <CancelButton text="出勤辞退" /></Box>
-            </Flex>
-          </SimpleGrid>
+      <Body title="出勤確定処理" statusMessage="出勤確定待ちの日程が3件あります">
+        <Box w={resp("100%", "80%", "80%")} mx="auto">
+          <VStack
+            divider={<StackDivider borderColor="gray.200" />}
+            spacing={3}
+            align="stretch"
+          >
+            <SimpleGrid columns={{ sm: 1, md: 3, lg: 3 }} gridTemplateColumns={{ sm: "", md: "3.5fr 3.5fr 3fr", lg: "2.5fr 4.5fr 3fr" }} alignItems="center">
+              <Text className="kb" mx={resp("auto", 0, 0)} fontSize={resp("1rem", "1.2rem", "1.2rem")} textAlign="right">2022/12/11 (日)</Text>
+              <Text className="kr" mx={resp("auto", 0, 0)} px={resp("auto", 3, 3)} fontSize={resp("0.65rem", "0.70rem", "0.75rem")} textAlign={responsiveType === "SmartPhone" ? "center" : "left"} color="#5f5f5f">12月シフト募集</Text>
+              <Flex w="100%" mt={resp("0.5rem", 0, 0)} justifyContent={responsiveType === "SmartPhone" ? "center" : "end"} alignItems="center">
+                <Button mr={resp(2, 3, 3)} size="sm" colorScheme="whatsapp" variant="outline">確定する</Button>
+                <Button ml={resp(2, 3, 3)} size="sm" colorScheme="red" variant="outline">拒否する</Button>
+              </Flex>
+            </SimpleGrid>
 
-          <SimpleGrid w={resp("100%", "80%", "80%")} mx="auto" my={5} py={3} columns={{ sm: 1, md: 2, lg: 2 }} spacingX={5} justifyItems="center" alignItems="center" boxShadow="xl" borderRadius={15} borderTop="solid 3px #718eed">
-            <Box textAlign={responsiveType === "SmartPhone" ? "center" : "right"}>
-              <Text className="kr" maxW={resp("17rem", "13.5rem", "18rem")} fontSize={resp("0.75rem", "0.80rem", "0.85rem")} color="#5f5f5f">大学OC(プレ入試)スタッフ募集</Text>
-              <Text className="kb" fontSize="1.2rem">2022/12/04 (日)</Text>
-            </Box>
-            <Flex mr={2} mt={resp(2, 0, 0)}>
-              <Box mr={3}><ConfirmButton text="出勤確定" /></Box>
-              <Box ml={3}> <CancelButton text="出勤辞退" /></Box>
-            </Flex>
-          </SimpleGrid>
-        </>
+            <SimpleGrid columns={{ sm: 1, md: 3, lg: 3 }} gridTemplateColumns={{ sm: "", md: "3.5fr 3.5fr 3fr", lg: "2.5fr 4.5fr 3fr" }} alignItems="center">
+              <Text className="kb" mx={resp("auto", 0, 0)} fontSize={resp("1rem", "1.2rem", "1.2rem")} textAlign="right">2022/12/04 (日)</Text>
+              <Text className="kr" mx={resp("auto", 0, 0)} px={resp("auto", 3, 3)} fontSize={resp("0.65rem", "0.70rem", "0.75rem")} textAlign={responsiveType === "SmartPhone" ? "center" : "left"} color="#5f5f5f">大学OC(プレ入試)スタッフ募集</Text>
+              <Flex w="100%" mt={resp("0.5rem", 0, 0)} justifyContent={responsiveType === "SmartPhone" ? "center" : "end"} alignItems="center">
+                <Button mr={resp(2, 3, 3)} size="sm" colorScheme="whatsapp" variant="outline">確定する</Button>
+                <Button ml={resp(2, 3, 3)} size="sm" colorScheme="red" variant="outline">拒否する</Button>
+              </Flex>
+            </SimpleGrid>
+
+            <SimpleGrid columns={{ sm: 1, md: 3, lg: 3 }} gridTemplateColumns={{ sm: "", md: "3.5fr 3.5fr 3fr", lg: "2.5fr 4.5fr 3fr" }} alignItems="center">
+              <Text className="kb" mx={resp("auto", 0, 0)} fontSize={resp("1rem", "1.2rem", "1.2rem")} textAlign="right">2022/12/31 (土)</Text>
+              <Text className="kr" mx={resp("auto", 0, 0)} px={resp("auto", 3, 3)} fontSize={resp("0.65rem", "0.70rem", "0.75rem")} textAlign={responsiveType === "SmartPhone" ? "center" : "left"} color="#5f5f5f">妙に文字数が多くて今にも見切れそうなアンケートタイトル</Text>
+              <Flex w="100%" mt={resp("0.5rem", 0, 0)} justifyContent={responsiveType === "SmartPhone" ? "center" : "end"} alignItems="center">
+                <Button mr={resp(2, 3, 3)} size="sm" colorScheme="whatsapp" variant="outline">確定する</Button>
+                <Button ml={resp(2, 3, 3)} size="sm" colorScheme="red" variant="outline">拒否する</Button>
+              </Flex>
+            </SimpleGrid>
+          </VStack>
+        </Box>
       </Body>
     </>
   )
