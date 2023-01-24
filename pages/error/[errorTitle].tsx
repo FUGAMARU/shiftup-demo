5200976d26292224525ec4a023d24f52de9fc22a
FUGAMARU <gofugashi@gmail.com>
2023-01-24 16:20:42 +0900

    [Add] オリジナルの404ページを追加

--- pages/error/[errorTitle].tsx
+++ pages/error/[errorTitle].tsx
@@ -4,17 +4,11 @@ import { useRouter } from "next/router"
 import Head from "next/head"
 
 // Chakra UI Components
-import { Box, Alert, AlertIcon, AlertTitle, AlertDescription } from "@chakra-ui/react"
+import { Box } from "@chakra-ui/react"
 
 // Custom Components
 import Header from "components/header/Header"
-import SimpleButton from "components/button/SimpleButton"
-
-// Libraries
-import { faRightToBracket } from "@fortawesome/free-solid-svg-icons"
-
-// Functions
-import { resp } from "ts/functions"
+import ErrorView from "components/ErrorView"
 
 // Importing Defined Errors
 import * as fs from "fs"
@@ -36,7 +30,7 @@ export const getStaticPaths: GetStaticPaths<Params> = async () => {
 }
 
 export const getStaticProps = async () => {
-  const jsonPath = path.join(process.cwd(), "json", "error.json")
+  const jsonPath = path.join(process.cwd(), "pages", "error", "errors.json")
   const jsonText = fs.readFileSync(jsonPath, "utf-8")
   const errors = JSON.parse(jsonText) as Errors
 
@@ -57,17 +51,7 @@ const Error: NextPage<Props> = ({ errors }) => {
 
       <Header />
 
-      <Box bg="#f5f5f7" pt={5}>
-        <Alert status="error" w={resp("95%", "80%", "75%")} mx="auto" boxShadow="xl" borderRadius="15px" border="solid 0.5px red">
-          <AlertIcon />
-          <AlertTitle className="kb">{errors[errorTitle as string].title}</AlertTitle>
-          <AlertDescription className="kr">{errors[errorTitle as string].detail}</AlertDescription>
-        </Alert>
-
-        <Box mt={5}>
-          <SimpleButton title="トップページに戻る" icon={faRightToBracket} onClick={() => router.push("/")} />
-        </Box>
-      </Box>
+      <ErrorView title={errors[errorTitle as string].title} description={errors[errorTitle as string].detail} />
     </Box>
   )
 }
